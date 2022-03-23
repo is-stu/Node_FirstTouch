58f9d3e76e2cd1867838c1220b98bc3a135efa07
Stewar Marin <stewar.marin@sofka.com.co>
2022-03-23 15:56:21 -0500

    Added Yargs and conditions by terminal

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 const { Multiply } = require('./helpers/multiply.js');
+const argv = require('yargs')
+  .options({
+    b: {
+      alias: 'base',
+      type: 'number',
+      demandOption: true,
+    },
+    l: {
+      alias: 'listar',
+      type: 'boolean',
+      default: false,
+    },
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) {
+      throw 'La base debe de ser un numero';
+    }
+    return true;
+  }).argv;
 
 //Traer las variables sobre la linea de comandos
 // const [, , base = 'base=5'] = process.argv;
@@ -18,4 +37,10 @@ const { Multiply } = require('./helpers/multiply.js');
 //   readline.close();
 // });
 
+// // console.log(process.argv);
+const { b, l } = argv;
+// console.log(`La base es ${b}`); // Crea un objeto en el cual contiene los parametros que se les pasa en la console
 
+Multiply(b, l)
+  .then((fileName) => console.log(`${fileName} creado`))
+  .catch((error) => console.log(error));
--- helpers/multiply.js
+++ helpers/multiply.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 
 const table = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const Multiply = async (number = 1) => {
+const Multiply = async (number = 1, listar = false) => {
+  let title = '';
   try {
     let result = '';
     result += `----- Table of ${number} -----\n`;
     table.map((tNumber) => (result += `${tNumber * number}\n`));
     result += `-----            -----\n`;
-    const title = `table of ${number}`;
+    listar && console.log(result);
+    title = `table of ${number}`;
     fs.writeFileSync(title, result);
   } catch (error) {
     throw error;
